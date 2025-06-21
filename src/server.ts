e85442ca15bdbd88f11eab373ec207a4d344390a
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-22 01:30:47 +0600

    basic server created with mongoose ,express and typescript

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { Server } from "./../node_modules/@types/connect/index.d";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "./app";
+dotenv.config();
+
+const port = process.env.port || 5000;
+
+async function main() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xnok4yx.mongodb.net/book-nest?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("✅ Connected to MongoDB using mongoose");
+    const server: Server = app.listen(port, () => {
+      console.log(`🔥 Note server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main()
